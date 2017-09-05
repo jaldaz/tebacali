cc45e1686cbe1244ffd62710e8b8d2fb9b4b06bd
jaldaz <jorge@shifteight.com>
2017-09-05 15:34:17 -0300

    first version

---
+++ application/views/main/index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Caliteba - Caliteba Web</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo base_url("assets/css/font-awesome.min.css"); ?>" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="<?php echo base_url("assets/css/magnific-popup.css"); ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url("assets/css/creative.min.css"); ?>" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Caliteba</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">Nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">Servicios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Productos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead">
+      <div class="header-content">
+        <div class="header-content-inner">
+          <h1 id="homeHeading">Caliteba</h1>
+          <hr>
+          <p>arcu phasellus ut commodo venenatis maecenas id nunc at porta</p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Averigua Mas!</a>
+        </div>
+      </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">Nosotros</h2>
+            <hr class="light">
+            <p class="text-faded">Lorem ipsum dolor. Sit amet interdum tristique platea mattis. Dictum in enim. Cras eget posuere. Quis ridiculus erat amet diam sed. Nullam praesent curabitur lectus curabitur ac id nonummy dui. Suspendisse sit mauris sodales nunc pellentesque. Pulvinar sapien praesentium curabitur sociosqu in. Sagittis tincidunt erat arcu viverra lectus felis leo.</p>
+            <a class="btn btn-default btn-xl js-scroll-trigger" href="#services">Servicios</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">A su servicio</h2>
+            <hr class="primary">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-diamond text-primary sr-icons"></i>
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
+              <h3>Ready to Ship</h3>
+              <p class="text-muted">You can use this theme as is, or you can make changes!</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
+              <h3>Up to Date</h3>
+              <p class="text-muted">We update dependencies to keep things fresh.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
+              <h3>Made with Love</h3>
+              <p class="text-muted">You have to make your websites with love these days!</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#portfolio">Productos</a>
+          </div>
+        </div>
+      </div>        
+    </section>
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid">
+        <div class="row no-gutter popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php echo base_url("assets/img/portfolio/fullsize/1.jpg"); ?>">
+              <img class="img-fluid" src="<?php echo base_url("assets/img/portfolio/thumbnails/1.jpg"); ?>" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project Name
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php echo base_url("assets/img/portfolio/fullsize/2.jpg"); ?>">
+              <img class="img-fluid" src="<?php echo base_url("assets/img/portfolio/thumbnails/2.jpg"); ?>" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project Name
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php echo base_url("assets/img/portfolio/fullsize/3.jpg"); ?>">
+              <img class="img-fluid" src="<?php echo base_url("assets/img/portfolio/thumbnails/3.jpg"); ?>" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project Name
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php echo base_url("assets/img/portfolio/fullsize/4.jpg"); ?>">
+              <img class="img-fluid" src="<?php echo base_url("assets/img/portfolio/thumbnails/4.jpg"); ?>" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project Name
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php echo base_url("assets/img/portfolio/fullsize/5.jpg"); ?>">
+              <img class="img-fluid" src="<?php echo base_url("assets/img/portfolio/thumbnails/5.jpg"); ?>" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project Name
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php echo base_url("assets/img/portfolio/fullsize/6.jpg"); ?>">
+              <img class="img-fluid" src="<?php echo base_url("assets/img/portfolio/thumbnails/6.jpg"); ?>" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project Name
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--<div class="call-to-action bg-dark">
+      <div class="container text-center">
+        <h2>Free Download at Start Bootstrap!</h2>
+        <a class="btn btn-default btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/">Download Now!</a>
+      </div>
+    </div>-->
+
+    <section id="contact" class="bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Contactenos!</h2>
+            <hr class="primary">
+            <p>Quiere contratar nuestros servicios? Averiguar sobre nuestros productos? Envienos un email o llamenos!</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <i class="fa fa-phone fa-3x sr-contact"></i>
+            <p>+5411-4563-6789</p>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+            <p>
+              <a href="mailto:contacto@caliteba.com.ar">contacto@caliteba.com.ar</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url("assets/js/jquery.min.js"); ?>"></script>
+    <script src="<?php echo base_url("assets/js/popper.min.js"); ?>"></script>
+    <script src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url("assets/js/jquery.easing.min.js"); ?>"></script>
+    <script src="<?php echo base_url("assets/js/scrollreveal.min.js"); ?>"></script>
+    <script src="<?php echo base_url("assets/js/jquery.magnific-popup.min.js"); ?>"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url("assets/js/creative.min.js"); ?>"></script>
+
+  </body>
+
+</html>
